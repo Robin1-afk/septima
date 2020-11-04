993542072f0f356e6595ff8b7605d29b1bb711c9
robin <rovinelcapo@gmail.com>
2020-11-04 00:16:35 -0500

    frist commit

---
+++ include/slider.php
@@ -0,0 +1,43 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="style/js.js"></script>
+<link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+<div class="container">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="item">
+          <img src="img/curso.jpg" alt="" width="100%" height="510px">
+          </div>
+        </div>
+        <div class="carousel-item">
+            <div class="item">
+             <img src="img/curso.jpg" alt="" width="100%" height="510px">
+          </div>
+        </div>
+        <div class="carousel-item">
+            <div class="item">
+            <img src="img/curso.jpg" alt="" width="100%" height="510px">
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+</div>
+    
+</body>
---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="style/style.css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="style/js.js"></script>
+</head>
+<body>
+<div class="responsive">
+    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top" style="background-color: #4ca144!important;">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a href=""> <img src="" width="50px" alt=""> </a>
+        <a class="navbar-brand" rel="nofollow" target="_blank" href="">PLANES Y SERVICIOS</a>
+        <a class="navbar-brand" rel="nofollow" target="_blank" href="">NOVEDADES</a>
+        <a class="navbar-brand" rel="nofollow" target="_blank" href="">VISITAR</a>
+        <a class="navbar-brand" rel="nofollow" target="_blank" href="">HOME</a>
+        <a class="navbar-brand" rel="nofollow" target="_blank" href="">QUIENES SOMOS</a>
+        <a class="navbar-brand" rel="nofollow" target="_blank" href="">ENTRAR</a>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" rel="nofollow" target="_blank" href="http://scadonsak.com/7bF">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+  
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <div class="carousel-item active">
+            <img class="first-slide" src="img/imagen1.jpg" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption d-none d-md-block text-left">
+                <h1>UNA PUERTA ABIERTA A UN UNIVERSO DE POSIBILIDADES</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+   
+<center>      
+        <div class="container marketing">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+          
+          <div class="col-lg-6">
+            <img class="rounded " src="img/evento.JPG" alt="Generic placeholder image" width="480px" height="300">
+            <div class="container">
+              <div class="carousel-caption d-none d-md-block text-left">
+                <p><a class="btn btn-lg btn-success " href="#" role="button">Eventos</a></p>
+              </div>
+            </div>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-6">
+            <img class="rounded " src="img/curso.JPG" alt="Generic placeholder image" width="480px" height="300">
+            <div class="container">
+              <div class="carousel-caption d-none d-md-block text-left">
+                <p><a class="btn btn-lg btn-success " href="#" role="button">Eventos</a></p>
+              </div>
+            </div>
+          </div><!-- /.col-lg-4 -->
+
+          </div>
+      </center>
+      
+<hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-5">
+        <div class="container slider">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner slider">
+        <div class="carousel-item active">
+          <div class="item">
+          <img src="img/curso.jpg" alt="" width="100%" height="510px">
+          </div>
+        </div>
+        <div class="carousel-item">
+            <div class="item">
+             <img src="img/curso.jpg" alt="" width="100%" height="510px">
+          </div>
+        </div>
+        <div class="carousel-item">
+            <div class="item">
+            <img src="img/curso.jpg" alt="" width="100%" height="510px">
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+</div>
+    
+        </div>
+
+
+      </div>
+</div>
+      <hr class="featurette-divider">
+      <div class="container">
+
+      <div class="contenedor-caja-video">
+<div class="caja-video">
+<video class="video"  controls poster="img/er.jpg">
+    <source src="img/Video_prototipo.mp4" type="video/mp4">
+</video>
+</div>
+</div>
+
+<form action="#" target="" method="get" name="formDatosPersonales" >
+<br>
+<div class="formulario">
+<br>
+	<input class="form-control" type="text" name="nombre"  placeholder="Nombre" />
+<br>
+	<input class="form-control" type="text" name="apellidos" id="apellidos" placeholder="Email"/>
+<br>
+	<input class="form-control" type="email" name="email" id="email" placeholder="Espacio requerido" required />
+
+<br>
+	<textarea rows="5" class="form-control" name="mensaje" for="mensaje" placeholder="Mensaje" maxlength="300"></textarea>
+  <br>
+  <center>
+  <input class="btn btn-gree" type="submit" name="enviar" value="enviar datos"/>
+  </center>
+</div>
+</form>
+</div>
+</body>
+</html>
+
+    </div>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+  
+</body>
+</html>
